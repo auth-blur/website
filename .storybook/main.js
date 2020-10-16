75a100c807e907cdb1a0a14e2c149e81bdc81674
ali-furkqn <mail@alifurkan.codes>
2020-10-16 12:31:17 +0300

    🧧 Installation (storybook)
    Removed Begin Template Storybook instead of new installed storybook (But have some issues)

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,7 +1,27 @@
+const path = require("path")
+
 module.exports = {
+    webpackFinal: (config) => {
+        Object.assign(config.resolve.alias,{
+            "@styles": path.resolve(__dirname,"../styles/"),
+            "@components": path.resolve(__dirname,"../components/"),
+            "@layouts": path.resolve(__dirname,"../layouts/"),
+            "@config": path.resolve(__dirname,"../config/"),
+            "@lib": path.resolve(__dirname,"../lib/")
+        })
+        config.module.rules.push({
+            test: /\.css$/,
+            use: ['style-loader', 'css-loader?modules=true'],
+            include: path.resolve(__dirname, '../'),
+        })
+        return config
+    },
     stories: [
         "../stories/**/*.stories.mdx",
         "../stories/**/*.stories.@(js|jsx|ts|tsx)",
     ],
-    addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
+    addons: [
+        "@storybook/addon-links",
+        "@storybook/addon-essentials"
+    ],
 }
