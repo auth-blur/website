a321431ac7ff313181011791619546a397089a4d
ali-furkqn <mail@alifurkan.codes>
2020-10-16 12:34:47 +0300

    🎊Add (_app.js): Added new Meta tags

--- pages/_app.js
+++ pages/_app.js
@@ -5,6 +5,24 @@ const MyApp = ({ Component, pageProps }) => (
     <>
         <Head>
             <title>Picassco</title>
+            <meta name="author" content="Ali Furkan, alifurkan@picass.co" />
+            <meta
+                name="description"
+                content="Let's create your peerless profile!"
+            />
+            <meta name="og:title" content="Picassco" />
+            <meta name="og:site_name" content="Picassco" title="title" />
+            <meta name="og:url" content="https://picass.co" />
+            <meta name="theme-color" content="#F15137" />
+            <meta
+                name="keywords"
+                content="Picassco, Profile, Avatar, Picasso, Avatar Profile, Picassco Avatar, Picassco Profile"
+            />
+            <meta name="og:image" content="/assets/banner-wave.png" />
+            <meta
+                name="twitter:image"
+                content="/assets/brand/picassco-artwork.png"
+            />
         </Head>
         <Component {...pageProps} />
     </>
