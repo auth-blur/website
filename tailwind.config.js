529cf105308a3a43c164c573769bd6dcfe58a832
ali-furkqn <mail@alifurkan.codes>
2020-10-16 12:32:29 +0300

    🎨 Configuration (Tailwind)

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,7 +10,40 @@ module.exports = {
         fontFamily: {
             display: ["Inter", "sans-serif"],
         },
-        extend: {},
+        borderRadius: {
+            "none": "0",
+            "sm": "0.25rem",
+            "default": "1rem",
+            "md": "0.5rem",
+            "lg": "0.75rem",
+            "full": "9999px"
+        },
+        extend: {
+            colors: {
+                primary: {
+                    500: "#A4463F",
+                    400: "#F15137",
+                    300: "#F86F64",
+                    200: "#FF8278",
+                    100: "#FBA29B",
+                    "light": "#FED4D0"
+                },
+                secondary: {
+                    500: "#933759",
+                    400: "#EA226C",
+                    300: "#FD5392",
+                    200: "#FF8AB5",
+                    100: "#FFA6C7",
+                },
+                alt: {
+                    500: "#FFFFFF",
+                    400: "#FBA29B",
+                    300: "#FED4D0",
+                    200: "#FFFFFF",
+                    100: "#FFFFFF",
+                }
+            }
+        },
     },
     variants: {},
     plugins: [],
