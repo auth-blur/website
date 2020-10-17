67b59358597ecd171e6f0c85f26bee54da9bda8b
ali-furkqn <mail@alifurkan.codes>
2020-10-17 17:32:38 +0300

    🎨 Create (Landing Page):  temporary landing page

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,31 @@
-const Page = () => <h1>Hello World</h1>
+import Button from "@components/button"
+import Input from "@components/input"
+import SingleLayout from "@layouts/single-layout"
 
-export default Page
+const Page = () => {
+    const handle = async () => {
+        const data = await fetch("https://api.picass.co/v1/early/mail", {
+            method: "POST",
+        })
+        if (data.status > 199 && data.status < 299) {
+            //
+        } else {
+            //
+        }
+    }
+    return (
+        <>
+            <h1 className="font-extrabold text-5xl mx-auto mb-2 text-center max-w-md">
+                Join our early access list!
+            </h1>
+            <h2 className="font-medium text-xl mb-6 text-center">
+                All of your things, in one place
+            </h2>
+            <Input placeholder="Enter email to join ">
+                <Button label="Join" onClick={handle} />
+            </Input>
+        </>
+    )
+}
+
+export default SingleLayout(Page)
