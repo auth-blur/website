1100e2f2c5b6723f237f3dc2f9e784dcea2fbb43
ali-furkqn <mail@alifurkan.codes>
2020-10-17 17:33:27 +0300

    ⚙ Adjustment (Button)

--- components/button/index.js
+++ components/button/index.js
@@ -1,12 +1,14 @@
 import PropTypes from "prop-types"
 import style from "./style.module.css"
 
-const Button = ({ label, textColor, bgColor, Component, ...p }) => {
+const Button = ({ label, textColor, bgColor, size, Component, ...p }) => {
+    if (process.env.NODE_ENV === "storybook") import("react")
     const props = {
         className: [
             style.button,
-            `bg-${bgColor ?? "primary-400"}`,
-            `text-${textColor ?? "white"}`,
+            `bg-${bgColor}`,
+            `text-${textColor}`,
+            `text-${size}`,
         ].join(" "),
         children: label,
         ...p,
@@ -17,11 +19,18 @@ const Button = ({ label, textColor, bgColor, Component, ...p }) => {
     return <a {...props} />
 }
 
+Button.defaultProps = {
+    bgColor: "primary-400",
+    textColor: "white",
+    size: "lg",
+}
+
 Button.propTypes = {
     label: PropTypes.string,
     textColor: PropTypes.string,
     bgColor: PropTypes.string,
     Component: PropTypes.any,
+    size: PropTypes.string,
 }
 
 export default Button
